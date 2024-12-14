80e0fff87f7c0fbc00d97388cc123990a931012a
Aaron Christopher Tanhar <aaron.christopher@komunal.co.id>
2024-12-14 20:35:06 +0700

    feat: metadata component

---
+++ src/lib/components/seo/headMeta.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let title = 'Paste by LordRonz';
+	export let description =
+		'Type and share your text! Data is stored in the URL, with brotli compression';
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+   <meta name="description" content={description}>
+   <meta name="author" content="Aaron Christopher Tanhar">
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
-	import { Toaster } from "$lib/components/ui/sonner";
+	import { Toaster } from '$lib/components/ui/sonner';
 	import '../app.css';
+	import HeadMeta from '$lib/components/seo/headMeta.svelte';
 	let { children } = $props();
 </script>
 
 <ParaglideJS {i18n}>
 	<Toaster />
+	<HeadMeta />
 	{@render children()}
 </ParaglideJS>
