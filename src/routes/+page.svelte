9c14f662269f2f86503cdc7a9c17555efb944075
Aaron Christopher Tanhar <aaron.christopher@komunal.co.id>
2024-12-14 20:15:28 +0700

    feat: generate markdown support

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,12 @@
 	import { BrotliWorkerMessageType, type BrotliWorkerMessage } from '$lib/workers/type';
 	import { onMount, onDestroy } from 'svelte';
 	import { toast } from 'svelte-sonner';
+	import { CompressedOutputType } from './type';
 
 	let text = '';
 	let compressedText = '';
 	let url = '';
+	let outputType: CompressedOutputType = CompressedOutputType.URL;
 
 	let brotliWorker: Worker;
 
@@ -20,7 +22,15 @@
 
 				if (type === BrotliWorkerMessageType.COMPRESSED) {
 					compressedText = payload;
-					url = `https://${location.host}${location.pathname}#${compressedText}`;
+					const rawResult = `https://${location.host}${location.pathname}#${compressedText}`;
+					switch (outputType) {
+						case CompressedOutputType.URL:
+							url = rawResult;
+							break;
+						case CompressedOutputType.MD:
+							url = `[paste](${rawResult})`;
+							break;
+					}
 				} else if (type === BrotliWorkerMessageType.DECOMPRESSED) {
 					text = payload;
 				} else {
@@ -28,7 +38,7 @@
 				}
 			};
 
-			decompressInitialContent()
+			decompressInitialContent();
 		})();
 
 		return () => {
@@ -38,7 +48,7 @@
 
 	onDestroy(() => {
 		brotliWorker?.terminate();
-	})
+	});
 
 	function decompressInitialContent() {
 		const initialContent = location.hash.substring(1);
@@ -46,12 +56,16 @@
 		decompress(initialContent);
 	}
 
-	function compress() {
+	function compress(type = CompressedOutputType.URL) {
+		outputType = type;
 		brotliWorker?.postMessage({ type: BrotliWorkerMessageType.COMPRESS, payload: text });
 	}
 
 	function decompress(text?: string) {
-		brotliWorker?.postMessage({ type: BrotliWorkerMessageType.DECOMPRESS, payload: text ?? compressedText });
+		brotliWorker?.postMessage({
+			type: BrotliWorkerMessageType.DECOMPRESS,
+			payload: text ?? compressedText
+		});
 	}
 
 	async function copy() {
@@ -66,14 +80,15 @@
 </script>
 
 <svelte:window on:hashchange={decompressInitialContent} />
-<div class="flex h-screen flex-col">
+<div class="flex h-screen flex-col bg-gray-900">
 	<!-- Textarea wrapper -->
 	<div class="flex-grow">
 		<textarea
 			id="plaintext"
 			spellcheck="false"
-			class="h-full w-full resize-none bg-black p-2 font-mono text-gray-200 caret-pink-500"
+			class="h-full w-full resize-none rounded-lg border border-gray-700 bg-gray-900 p-4 font-mono text-gray-100 placeholder-gray-500 caret-pink-500 focus:border-pink-500 focus:outline-none focus:ring-2 focus:ring-pink-500"
 			bind:value={text}
+			placeholder="Start typing here..."
 		></textarea>
 	</div>
 
@@ -83,15 +98,21 @@
 			<a
 				class="cursor-pointer px-2 py-1 hover:bg-gray-600"
 				href="https://github.com/lordronz/paster"
-				>[github]
+			>
+				[github]
 			</a>
 		</div>
 
 		<div class={cn('space-x-2', compressedText ? 'hidden' : 'flex')}>
-			<button class="cursor-pointer px-2 py-1 hover:bg-gray-600" onclick={compress}
-				>[generate url]
+			<button class="cursor-pointer px-2 py-1 hover:bg-gray-600" onclick={() => compress()}>
+				[generate url]
 			</button>
-			<button class="cursor-pointer px-2 py-1 hover:bg-gray-600">[generate markdown link]</button>
+			<button
+				class="cursor-pointer px-2 py-1 hover:bg-gray-600"
+				onclick={() => compress(CompressedOutputType.MD)}
+			>
+				[generate markdown link]
+			</button>
 		</div>
 
 		<div
@@ -104,9 +125,9 @@
 				disabled
 			/>
 			<button class="cursor-pointer px-2 py-1 hover:bg-gray-600" onclick={copy}>[copy]</button>
-			<button class="cursor-pointer px-2 py-1 hover:bg-gray-600" onclick={cancelOffer}
-				>[cancel]</button
-			>
+			<button class="cursor-pointer px-2 py-1 hover:bg-gray-600" onclick={cancelOffer}>
+				[cancel]
+			</button>
 		</div>
 	</div>
 </div>
